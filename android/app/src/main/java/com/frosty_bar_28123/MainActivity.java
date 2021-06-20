8ccc6e46519f856d435041c2d010647b7e599ea1
Crowdbotics <team@crowdbotics.com>
2021-06-20 21:32:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/frosty_bar_28123/MainActivity.java
@@ -0,0 +1,15 @@
+package com.frosty_bar_28123;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "frosty_bar_28123";
+  }
+}
